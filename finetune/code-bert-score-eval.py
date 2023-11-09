78f107b7e92eb1bf27ed2bb99c6d918a9d7020ae
Hetarth Chopra <choprahetarth@gmail.com>
2023-11-08 23:02:13 -0600

    added code-bert-score

---
+++ finetune/code-bert-score-eval.py
@@ -0,0 +1,37 @@
+import code_bert_score
+
+def compute_similarity(code1, code2):
+    # The score function returns a 4-tuple of (precision, recall, F1, F3)
+    # We only need the F1 score for similarity, so we extract that
+    _, _, f1_score, _ = code_bert_score.score(cands=[code1], refs=[code2], lang='python')
+    return f1_score.item()  # Convert tensor to a standard Python number
+
+# example usage
+code1 = """
+---
+- hosts: your_host
+  become: yes
+  tasks:
+    - name: Update apt cache
+      apt:
+        update_cache: yes
+
+    - name: Install Nginx
+      apt:
+        name: nginx
+        state: present
+"""
+
+code2 = """
+---
+- hosts: your_host
+  become: yes
+  tasks:
+    - name: Update apt cache
+      shell: apt-get update
+
+    - name: Install Nginx
+      shell: apt-get install -y nginx
+"""
+
+print(compute_similarity(code1, code2))
